49845b0d9b9980a767e4dcfd48d43e98a90a5cac
Kushan Vora <kushpvo@gmail.com>
2019-12-08 19:37:14 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,81 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "http://twitter/"
+        },
+        {
+            "icon": "github-circled",
+            "url": "http://github"
+        },
+        {
+            "icon": "dribbble",
+            "url": "http://dribbble"
+        },
+        {
+            "icon": "skype",
+            "url": "htttp://skype"
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "http://linkedin"
+        }
+    ],
+    "experiences": [],
+    "name": "John Smith",
+    "header": "Hi, I'm Kushan Vora",
+    "cta": {
+        "label": "Get my resume",
+        "url": "resume.pdf"
+    },
+    "testimonials": [
+        {
+            "__key": "76d478b3-bc47-4e1e-8045-555724958ee4",
+            "quote": "Incredibly talented and hardworking. A super friendly guy who is a frequent communicator",
+            "name": "Person Name",
+            "title": "CEO at Company"
+        },
+        {
+            "__key": "f18b7860-1da4-4fc4-9f36-c795d582cbb5",
+            "quote": "Wow, thank you for this, you probably have the most intuitive explanation for this problem!",
+            "name": "Person Name"
+        }
+    ],
+    "title": "John Smith",
+    "projects": [
+        {
+            "__key": "c1893d76-a188-45d6-804a-23442db3413f",
+            "name": "Combustion",
+            "description": "Combustion is a sleek, modern web client for the transmission bittorrent client.",
+            "tags": [
+                "React",
+                "Javascript",
+                "Webpack",
+                "Mobx",
+                "CSSModules"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "//"
+        },
+        {
+            "__key": "01204ece-21ac-49c7-8b81-61b9530bd876",
+            "name": "Merc-01",
+            "description": "Merc-01 is a fast paced twin-stick shooter built on top of pyglet.",
+            "tags": [
+                "Python",
+                "WebGL",
+                "Pyglet"
+            ],
+            "img": "merc-01.png",
+            "url": "//"
+        }
+    ],
+    "email": "johnsmith@mail.com",
+    "description": "I'm a college junior currently taking up Computer Science in University X. I'm a freelance developer with 4 years of experience, having worked with clients such as X, Y, Z. I enjoy building everything from small business sites to rich interactive web apps.",
+    "footerTitle": "Software Engineer"
+}
